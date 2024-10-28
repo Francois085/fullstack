0750bf75c6446004f67f85f19db7019aa3424f1c
Francois085 <baronfrancois4329@neuf.fr>
2024-10-28 23:04:13 +0100

    router bis

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect('mongodb+srv://UserTest2:PasswordUserTest2@cluster0.ia9wl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -16,9 +17,11 @@ app.use((req, res, next) => {
     next();
   });
 
-const Thing = require('./models/thing');
+  //app.use(bodyParser.json());
+  
+  app.use('/api/stuff', stuffRoutes);
 
-app.post('/api/stuff', (req, res, next) => {
+/*app.post('/api/stuff', (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({
     ...req.body
@@ -50,28 +53,7 @@ app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
-});
+});*/
 
-/*app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });*/
 
 module.exports = app;
